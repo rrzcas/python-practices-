a92c7ee7ad70393074f883a1ff0ad4c089820e6d
rrzcas <razca216@gmail.com>
2025-04-27 12:03:07 +0100

    Add files via upload
    mt lec 16 - recursive on non numerics 3 exercises + 3 del bug exercises

---  mt_utryits_lec16.py
+++  mt_utryits_lec16.py
@@ -62,7 +62,7 @@ def my_deepcopy(L):
             return [L[0]]+my_deepcopy(L[1:])
 
 
-myL = ["abc", ['d'], ['e', ['f', 'g']]]
+myL = ["abc", ['d'], ['e', ['f', 'g',[['kk']]]]]
 my_newL = my_deepcopy(myL)
 print(myL)
 print(my_newL)
@@ -70,17 +70,67 @@ myL[2][1][0] = 1
 print(myL)      # should be ['abc', ['d'], ['e', [1, 'g']]]
 print(my_newL)  # should be ['abc', ['d'], ['e', ['f', 'g']]]
 
-    # if L==[]:
-    #     return []
-    # if len(L)==1:
-    #     if type(L[0])== list :
-    #         return [L[0]] or my_deepcopy(L[1:])
-    #     else:
-    #         return L[0]
-    # else:
-    #     if type(L[0])==list :
-    #         return L[0]
-    #     else:
-    #         return [my_deepcopy(L[0])]+[my_deepcopy(L[1:])]
 
 
+# Q4. Here are 3 recursive functions that are incorrectly implemented.
+# Debug them to have them do what the specs say.
+def f(L):
+    """ L is a non-empty list of lowercase letters.
+    Returns the letter earliest in the alphabet. """
+    if len(L) == 1:
+        return L[0]
+    else:
+        if L[0] > L[len(L)-1]: #b>a
+            return f(L[1:])
+        else:
+            return f(L[0:len(L)-1])            
+print(f(['z', 'k', 'b', 'c', 'd','b','a','s','p','l']))  # should print 'a'
+print(f([ 'k', 'c', 'a','b']))  # should print 'a'
+
+def g(L, e):
+    """ L is list of ints, e is an int
+    Returns a count of how many times e occurrs in L  """
+    if len(L) == 0:
+        return 0
+    elif len(L) == 1:
+        if e == L[0]:
+            return 1
+        else:
+            return 0
+    else:
+        if L[0] == e:
+            return(1+g(L[1:], e))
+        else:
+            return g(L[1:],e)
+# print(g([1,2,3,1], 1))     # should print 2
+print(g([1,1,2,3,1,1], 1)) # should print 4
+print(g([1,1,3,2,2,2,2,2], 2)) # prints 5
+
+
+def h(L, e):
+    """ L is list, e is an int
+    Returns a count of how many times e occurrs in L or 
+    (recursively) any sublist of L
+    """
+    if len(L) == 0:
+        return 0
+    if len(L)==1:
+        if type(L[0])==list:
+            return h(L[0],e)+h(L[1:], e)
+        else:
+             if L[0]==e:
+                return 1
+             else:
+                 return 0
+    else: # len(L[0])>1
+        if type(L[0])==list:
+            return h(L[0],e)+h(L[1:], e)
+        else:
+             if L[0]==e:
+                return 1+h(L[1:],e)
+             else:
+                 return h(L[1:],e)
+    
+print(h([1,2,[1,[1]],1], 1))        # should print 4
+print(h([1,2,[3,1,[1,[1]]]], 3))  # should print 1
+    
