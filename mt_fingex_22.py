a266140c1712b35eb79c78ded77e8b6594723054
rrzcas <razca216@gmail.com>
2025-05-12 15:20:49 +0100

    Add files via upload
    mit lec 22 - big oh and theta notation

---
+++ mt_fingex_22.py
@@ -0,0 +1,15 @@
+# mt_fingex_22
+
+# Question 1: Simplify n*n + log(n) + 2**a to determine θ in terms of n. 
+
+# - ans : n*n
+
+
+# Question 2: Simplify 2**n + n*log(n) + n**2 to determine θ in terms of n. 
+
+# - ans : 2**n
+
+
+# Question 3: Simplify f*log(f) + 100000 + 300*a + x*y*z to determine θ in terms of n
+
+# - ans : 1
